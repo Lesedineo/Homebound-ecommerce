65ac1912eb45c63f540c807a94701e42e074ea3a
Lesedineo <133144071+Lesedineo@users.noreply.github.com>
2026-04-21 16:54:15 +0200

    updated the css for the navigation bar

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
 import { ShopContext } from "../Context/ShopContext";
 import { Breadcrumb } from "../Components/Breadcrumbs/Breadcrumb";
 
